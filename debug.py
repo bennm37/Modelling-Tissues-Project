62caf9d5e2c865a10c3076f334c10daea864e6df
bennm37 <79370760+bennm37@users.noreply.github.com>
2021-11-17 09:44:56 +0000

    Parameter Dictionary Input

--- debug.py
+++ debug.py
@@ -3,4 +3,4 @@ def N_given_packing_fraction(phi,box_width):
     N = np.floor((phi*box_width**2)/np.pi)
     return int(N)
 box_width = 20
-glass = {"N":N_given_packing_fraction(1,box_width),"v_0":0.01,"D":0.1}
+glass = {"N":N_given_packing_fraction(1,box_width),"v_0":0.01,"D":0.1} 
