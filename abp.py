d5c776e03ef1daa7b7785fd2845c7701e7a1c298
bennm37 <79370760+bennm37@users.noreply.github.com>
2021-12-01 14:25:56 +0000

    Reading in Data Fixed

--- abp.py
+++ abp.py
@@ -4,7 +4,6 @@ import numpy.linalg as lag
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 from matplotlib.patches import Polygon,Ellipse
-from IPython.display import HTML
 import pandas as pd
 import os  
 
@@ -15,6 +14,14 @@ def short_scale_repulsion(pvec,R=0.1,k=1):
     return f_ij
 
 def repulsion_cohesion_potential(pvec,R=1,k=1,epsilon=0.15):
+    """Uses potential from the glassy behaviour paper[1], setting b_i =1 """
+    dist = lag.norm(pvec,axis=2)
+    f_ij = np.where(dist<R+R*epsilon,k*(R*dist-np.full(dist.shape,R**2)),np.zeros(dist.shape))
+    ##TODO convoluted way of saying r<d<r_2 to get into one condition for where
+    f_ij = np.where(np.abs(dist-(R+3*R*epsilon/2))<(R*epsilon/2),-k*(R*dist-np.full(dist.shape,(1+2*epsilon)*R**2)),f_ij)
+    return f_ij
+
+def repulsion_cohesion_potential2(pvec,R=1,k=1,epsilon=0.15,delta=0.2):
     """Uses potential from the glassy behaviour paper[1], setting b_i =1 """
     dist = lag.norm(pvec,axis=2)
     f_ij = np.where(dist<R+R*epsilon,k*(R*dist-np.full(dist.shape,R**2)),np.zeros(dist.shape))
@@ -113,45 +120,58 @@ class ABP(object):
         out = "__".join([key+"_"+str(p_dict[key]) for key in p_dict])
         return out
 
-    def generate_csv(self,T,dt,sample_rate):
+    def generate_csv(self,T,dt,sample_rate,folder_name = None):
         """Makes a folder named after parameter values 
         and saves the positions velocities and directions 
         at each sample rate dts"""
-        folder_name = "abp_data__"+self.get_parameter_suffix()
+        if folder_name:
+            folder_name = folder_name
+        else:
+            folder_name = "abp_data__"+self.get_parameter_suffix()
         os.mkdir(f"./data/{folder_name}")
         num_samples = int(np.floor(T//sample_rate))
         columns = ["x1","x2","d1","d2","v1","v2"]
         data = np.append(self.r,self.directions(),axis=1)
         data = np.append(data,self.rdot,axis=1)
         data_frame = pd.DataFrame(data,columns=columns)
-        data_frame.to_csv(f"./data/{folder_name}/dat_0.csv")
+        data_frame.to_csv(f"./data/{folder_name}/data_0.csv")
         for i in range(1,num_samples):
             for j in range(sample_rate):
                 self.update(dt)
             data = np.append(self.r,self.directions(),axis=1)
             data = np.append(data,self.rdot,axis=1)
             data_frame = pd.DataFrame(data,columns=columns)
-            data_frame.to_csv(f"./data/{folder_name}/dat_{i}.csv")
-
-
-
-##DISPLAYERS
-    def display_state(self):
-        fig,ax = plt.subplots()
-        s = ax.scatter(self.r[:,0],self.r[:,1])
-        q = ax.quiver(self.r[:,0],self.r[:,1],self.directions()[:,0],self.directions()[:,1],
-            headlength=10,headaxislength=10,scale=15/self.R)
+            data_frame.to_csv(f"./data/{folder_name}/data_{i}.csv")
+        return folder_name
 
 class Analysis(object):
-    def __init__(self,data_name,parameters,T,dt):
-        df = pd.read_csv(data_name)
+    def __init__(self,data_name,parameters,T,dt,n_saves,data_type ="ben"):
         self.T = T
         self.dt = dt
+        self.n_saves = n_saves
         self.N = parameters["N"]
-        self.r_data = np.array(df[["x1","x2"]]).reshape(T+1,self.N,2)
-        self.v_data = np.array(df[["v1","v2"]]).reshape(T+1,self.N,2)
-        self.d_data = np.array(df[["d1","d2"]]).reshape(T+1,self.N,2)
         self.p_dict = parameters
+
+        if data_type == "ben":
+            filenames = [f"dat_{i}" for i in range(self.n_saves)]
+            frames = [pd.read_csv(f"{data_name}/{filename}.csv") for filename in filenames]
+            df = pd.concat(frames)
+            self.r_data = np.array(df[["x1","x2"]]).reshape(self.n_saves,self.N,2)
+            self.v_data = np.array(df[["v1","v2"]]).reshape(self.n_saves,self.N,2)
+            self.d_data = np.array(df[["d1","d2"]]).reshape(self.n_saves,self.N,2)
+        
+        if data_type == "pyABP":
+            dat_content = [i.strip().split() for i in open(f"{data_name}/data1.dat")]
+            columns = dat_content[0]
+            data = np.zeros((self.n_saves,self.N,8))
+            for i in range(0,self.n_saves):
+                dat_i = [i.strip().split() for i in open(f"{data_name}/data{i}.dat")]
+                data[i,:,:] = dat_i[1:]
+            df = pd.DataFrame(data.reshape(self.N*self.n_saves,8),columns=columns)
+            self.r_data = np.array(df[["x1","x2"]]).reshape(self.n_saves,self.N,2)
+            self.v_data = np.array(df[["v1","v2"]]).reshape(self.n_saves,self.N,2)
+            self.theta_data = np.array(df["theta"]).reshape(self.n_saves,self.N,1)
+            self.d_data = np.append(np.cos(self.theta_data),np.sin(self.theta_data),axis = 2)
 
     def analytic_msd(self,num_time_steps):
         tau_r =1 #what is persistence time?
@@ -159,21 +179,38 @@ class Analysis(object):
         4*self.p_dict["D"]+2*self.p_dict["v_0"]**2*tau_r*(interval-tau_r*(1-np.exp(-interval/tau_r)))
     
     def msd(self,m):
-        msd = np.sum(np.sum((self.r_data[m:,:]-self.r_data[:(self.T-m),:])**2,axis=1))/(self.T-m)
+        i_sum = np.sum((self.r_data[m:,:]-self.r_data[:self.T-m,:])**2,axis=1)
+        msd = np.sum(i_sum)/(self.T-m)
         return msd
     
-    def generate_msd_data(self,m_range):
+    def generate_msd_data(self):
+        m_range = np.linspace(0,self.T,self.T,dtype=int)
         msd_data = np.zeros(m_range.shape)
         for i,m in enumerate(m_range):
+            if self.msd(m)==np.nan:
+                print(m)
             msd_data[i] = self.msd(m)
         return msd_data
 
-    def animate_movement_patch(self,patch_type="circle"):
+    def plot_frame(self,frame_no):
+        asp = [10,10,15]
+        r_data,direction_data,velocity_data = self.r_data,self.d_data,self.v_data
+        fig,ax = plt.subplots()
+        fig.set_size_inches(8,8)
+        ax.set(xlim=(0,self.p_dict["box_width"]),ylim=(0,self.p_dict["box_width"]))
+        directions = ax.quiver(r_data[frame_no,:,0],r_data[frame_no,:,1],direction_data[frame_no,:,0],direction_data[0,:,1],
+            headaxislength=asp[0],headlength=asp[1],scale=asp[2])
+        velocities = ax.quiver(r_data[frame_no,:,0],r_data[frame_no,:,1],velocity_data[frame_no,:,0],velocity_data[0,:,1],color="r",
+            headaxislength=asp[0],headlength=asp[1],scale=asp[2])
+        for cell in r_data[frame_no,:,:]:
+            c = Ellipse(cell,1,1,fill=False,color="k")
+            p = ax.add_patch(c)
+
+    def animate_movement_patch(self,sample_rate =10,patch_type="circle"):
         """Animates the movement of the active brownian particles using a 
         matplotlib quiver plot"""
         ##TODO sort arrow scaling
         asp = [10,10,15] ##arrow shape parameters
-        sample_rate = 10
         r_data,direction_data,velocity_data = self.r_data,self.d_data,self.v_data
         fig,ax = plt.subplots()
         fig.set_size_inches(8,8)
@@ -182,12 +219,14 @@ class Analysis(object):
             headaxislength=asp[0],headlength=asp[1],scale=asp[2])
         velocities = ax.quiver(r_data[0,:,0],r_data[0,:,1],velocity_data[0,:,0],velocity_data[0,:,1],color="r",
             headaxislength=asp[0],headlength=asp[1],scale=asp[2])
+        ##This creates circles with diameter 1, not radius 1
         for cell in r_data[0,:,:]:
             c = Ellipse(cell,1,1,fill=False,color="k")
             p = ax.add_patch(c)
         def update_anim(i):
             sample = i*sample_rate
             ax.clear()
+            ax.set(title=f"frame no {i}")
             ax.set(xlim=(0,self.p_dict["box_width"]),ylim=(0,self.p_dict["box_width"]))
             directions = ax.quiver(r_data[sample,:,0],r_data[sample,:,1],direction_data[sample,:,0],direction_data[sample,:,1],
                 headaxislength=asp[0],headlength=asp[1],scale=asp[2])
@@ -196,6 +235,6 @@ class Analysis(object):
             for cell in r_data[sample,:,:]:
                 c = Ellipse(cell,1,1,fill=False,color="k")
                 p = ax.add_patch(c)
-        anim = animation.FuncAnimation(fig,update_anim,frames=self.T//sample_rate)
+        anim = animation.FuncAnimation(fig,update_anim,frames=self.n_saves//sample_rate)
         return anim
         
